c33a504cc19d0d18306ee99f8801321173ba563a
devR2001 <144663791+devR2001@users.noreply.github.com>
2023-10-16 21:09:35 +0200

    dev

--- src/App.js
+++ src/App.js
@@ -1,14 +1,50 @@
 import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [count, setCount] = useState(0)
+  const [text, setText] = useState('')
+
+  function handleIncrement() {
+    setCount(count + 1)
+  }
+
+  function handleDecrement() {
+    if (count > 0){
+      setCount(count - 1 )
+    } else {
+      setCount(0)
+    }
+  }
+
+  function handleButtonClick() {
+    alert("Button wurde geklickt")
+  }
+
+  function handleTextChange(e) {
+    setText(e.target.value)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hallo, ich lerne React!
         </p>
+        <button onClick={handleButtonClick}>Click mich Button</button>
+        <button onClick={handleIncrement}>Erhöhen</button>
+        <p>Zähler: {count}</p>
+        <button onClick={handleDecrement}>Verringern</button>
+        <input
+          type="text"
+          placeholder='Gib hier etwas ein'
+          value={text}
+          onChange={handleTextChange}
+        />
+          <p>Eingegebener Text: {text}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
