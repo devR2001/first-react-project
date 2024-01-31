f7ffac2e544554d82281016a03ce869a455d78b6
devR2001 <144663791+devR2001@users.noreply.github.com>
2024-01-31 19:19:59 +0100

    Added a Test in this Unit

--- src/App.jsx
+++ src/App.jsx
@@ -51,3 +51,24 @@ function App() {
 }
 
 export default App;
+
+// Test
+/*
+import React from 'react';
+
+
+export default function App() {
+    const [price, setPrice] = React.useState(100);
+    
+    function handleClicker(){
+        setPrice(75)
+    }
+    
+    return (
+        <div>
+            <p data-testid="price">${price}</p>
+            <button onClick={handleClicker}>Apply Discount</button>
+        </div>
+    );
+}
+*/
